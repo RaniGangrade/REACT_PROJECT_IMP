9bf40b1871b93d6220debb2140e61b97f93e4d0e
RaniGangrade <143512065+RaniGangrade@users.noreply.github.com>
2023-10-16 15:29:10 +0530

    Update App.js

--- project_04/App.js
+++ project_04/App.js
@@ -1,1 +1,16 @@
+import './App.css';
+import CurrencyConvertor from './CurrencyConvertor';
+function App() {
+  return (
+    <div className="App">
+      <Colorchanger />
+      <User massage ="hello"/>
+      <Form />
+      <PasswordGanrator />
+      <CurrencyConvertor />
+    </div>
+  );
+}
+
+export default App;
 
