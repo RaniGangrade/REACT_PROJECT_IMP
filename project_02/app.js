78d3d598c82e4d427b55c33a8157e67492555851
RaniGangrade <143512065+RaniGangrade@users.noreply.github.com>
2023-10-16 13:57:29 +0530

    Update app.js

--- project_02/app.js
+++ project_02/app.js
@@ -1,1 +1,18 @@
+import './App.css';
+import User from './User';
+import Form from './Form';
+import Colorchanger from './ColorChanger';
+import PasswordGanrator from './PasswordGenrator';
+function App() {
+  return (
+    <div className="App">
+      <Colorchanger />
+      <User massage ="hello"/>
+      <Form />
+      <PasswordGanrator />
+    </div>
+  );
+}
+
+export default App;
 
