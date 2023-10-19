ed3113f0203ee6de743824b1b7fbfd721f9aa71b
RaniGangrade <143512065+RaniGangrade@users.noreply.github.com>
2023-10-19 17:24:40 +0530

    Update App.js

--- project_05/App.js
+++ project_05/App.js
@@ -1,1 +1,18 @@
+import './App.css'
+import AddTodo from './Components/AddTodo';
+import Todos from './Components/Todos';
+
+const App = () => {
+  
+
+  return (
+    <div className="App">
+
+      <AddTodo/>
+      <Todos/>
+    </div>
+  );
+};
+
+export default App;
 
